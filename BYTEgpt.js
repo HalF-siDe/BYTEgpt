f0f3ad78f5a4b880d7595d8a4875c2be2c1bb238
HalF-siDe <150458877+HalF-siDe@users.noreply.github.com>
2023-11-11 01:38:03 -0500

    Update BYTEgpt.js

--- BYTEgpt.js
+++ BYTEgpt.js
@@ -1,14 +1,19 @@
-const renameElement = document.querySelector('.font-semibold');
-if (renameElement) {
-    renameElement.textContent = 'BYTE.dev';
-}
+javascript:(function(){
+  const e = document.querySelector('.font-semibold');
+  if (e) {
+    e.textContent = 'BYTE.dev';
+  }
 
-const element = document.querySelector('.flex.h-full.w-full.pb-2.md\\:pb-\\[8vh\\] h1.text-4xl.font-semibold.text-center.text-gray-200.dark\\:text-gray-600.ml-auto.mr-auto.mb-10.sm\\:mb-16.flex.gap-2.items-center.justify-center.flex-grow');
-if (element) {
-    element.remove();
-}
+  const t = document.querySelector('.flex.h-full.w-full.pb-2.md\\:pb-\\[8vh\\] h1.text-4xl.font-semibold.text-center.text-gray-200.dark\\:text-gray-600.ml-auto.mr-auto.mb-10.sm\\:mb-16.flex.gap-2.items-center.justify-center.flex-grow');
+  if (t) {
+    t.remove();
+  }
 
-const imageElement = document.querySelector('img[data-nimg="1"]');
-if (imageElement) {
-    imageElement.src = 'https://raw.githubusercontent.com/HalF-siDe/BYTEgpt/main/BYTE.ico';
-}
+  const n = document.querySelector('img[data-nimg="1"]');
+  if (n) {
+    n.src = 'https://raw.githubusercontent.com/HalF-siDe/BYTEgpt/main/BYTE.ico';
+    alert('Bookmarklet works!');
+  } else {
+    alert('Check the Discord/GitHub for updates');
+  }
+})();
