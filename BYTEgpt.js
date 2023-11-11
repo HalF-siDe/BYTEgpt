fbe665df5ce86d2a0adef2e1fabed44c99e17b65
HalF-siDe <150458877+HalF-siDe@users.noreply.github.com>
2023-11-11 01:29:03 -0500

    Update BYTEgpt.js

--- BYTEgpt.js
+++ BYTEgpt.js
@@ -1,9 +1,14 @@
 const renameElement = document.querySelector('.font-semibold');
 if (renameElement) {
-  renameElement.textContent = 'BYTE.dev';
+    renameElement.textContent = 'BYTE.dev';
 }
 
 const element = document.querySelector('.flex.h-full.w-full.pb-2.md\\:pb-\\[8vh\\] h1.text-4xl.font-semibold.text-center.text-gray-200.dark\\:text-gray-600.ml-auto.mr-auto.mb-10.sm\\:mb-16.flex.gap-2.items-center.justify-center.flex-grow');
 if (element) {
-  element.remove();
+    element.remove();
+}
+
+const imageElement = document.querySelector('img[data-nimg="1"]');
+if (imageElement) {
+    imageElement.src = 'https://raw.githubusercontent.com/HalF-siDe/BYTEgpt/main/BYTE.ico';
 }
